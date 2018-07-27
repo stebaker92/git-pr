1a47b47b49eec1c675fc903715330d4fd9a56a3f
Steve Baker <ste.baker.92@gmail.com>
2018-07-27 13:30:05 +0100

    wrap code in a function

--- git-pr.ps1
+++ git-pr.ps1
@@ -1,57 +1,60 @@
-$baseOverride = $args[0]
+function git-pr {
 
-$base = git config --get git-pr.base
+    $baseOverride = $args[0]
 
-if ($baseOverride -eq $null) {
-    if ($base -eq $null) {
-        Write-Host "Enter a base branch"
-        $base = Read-Host 
-        if ($base -eq $Null -or $base -eq "") {
-            $base = "master"
+    $base = git config --get git-pr.base
+
+    if ($baseOverride -eq $null) {
+        if ($base -eq $null) {
+            Write-Host "Enter a base branch"
+            $base = Read-Host 
+            if ($base -eq $Null -or $base -eq "") {
+                $base = "master"
+            }
+            git config git-pr.base $base
         }
-        git config git-pr.base $base
     }
+    else {
+        Write-Output "override detected"
+        Write-Output $baseOverride
+
+        git config git-pr.base $baseOverride
+        $base = $baseOverride
+    }
+
+    $branch = git symbolic-ref head --short
+
+    $repodir = git rev-parse --show-toplevel
+
+    $repo = Split-Path $repodir -Leaf
+
+    $origin = git remote get-url origin
+
+    if ($origin -eq $null) {
+        Write-Error "no origin found"
+        return;
+    }
+
+    # Remove the repo from the remote URL
+    $split = $origin.split("/")
+    $remoteWithoutRepoLength = $split.Length - 2;
+
+    $split = $split[0..$remoteWithoutRepoLength]
+
+    $remoteWithoutRepo = ($split -join "/") + "/"
+
+    if ($origin.Contains("://github") -eq $True) {
+        $url = $remoteWithoutRepo + $repo + "/compare/$base..." + $branch
+        #+ "?w=1" #w=1 removes whitespace
+    }
+    elseif ($origin.Contains("://gitlab") -eq $True) {
+        "this is a gitlab repo"
+        $url = $remoteWithoutRepo + $repo + "/merge_requests/new?merge_request%5Bsource_branch%5D=$branch&merge_request%5Btarget_branch%5D=master"
+    }
+    else {
+        Write-Error "$origin not supported"
+        return;
+    }
+
+    Start-Process $url
 }
-else {
-    Write-Output "override detected"
-    Write-Output $baseOverride
-	
-    git config git-pr.base $baseOverride
-    $base = $baseOverride
-}
-
-$branch = git symbolic-ref head --short
-
-$repodir = git rev-parse --show-toplevel
-
-$repo = Split-Path $repodir -Leaf
-
-$origin = git remote get-url origin
-
-if ($origin -eq $null) {
-    Write-Error "no origin found"
-    return;
-}
-
-# Remove the repo from the remote URL
-$split = $origin.split("/")
-$remoteWithoutRepoLength = $split.Length - 2;
-
-$split = $split[0..$remoteWithoutRepoLength]
-
-$remoteWithoutRepo = ($split -join "/") + "/"
-
-if ($origin.Contains("://github") -eq $True) {	
-    $url = $remoteWithoutRepo + $repo + "/compare/$base..." + $branch 
-    #+ "?w=1" #w=1 removes whitespace
-}
-elseif ($origin.Contains("://gitlab") -eq $True) {
-    "this is a gitlab repo"
-    $url = $remoteWithoutRepo + $repo + "/merge_requests/new?merge_request%5Bsource_branch%5D=$branch&merge_request%5Btarget_branch%5D=master"
-}
-else {
-    Write-Error "$origin not supported"
-    return;
-}
-
-Start-Process $url
