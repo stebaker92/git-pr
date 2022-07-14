58796bebac9e4a9ca1e0eb1c4fd1bc9a0795559f
Steve Baker <stebaker92@users.noreply.github.com>
2022-07-14 22:43:20 +0100

    .

--- tests/github-tests.ps1
+++ tests/github-tests.ps1
@@ -1,12 +1,13 @@
 Import-Module ".\git-pr.psm1" -Force
 
-$branch = "feature1"
-$base = "master";
+$global:branch = "feature1"
+$global:base = "master"
 
 Describe "GitHub" {
     Context "https pr" {
         it "should have correct url" {
             write-host "branch inside test is $global:branch"
+            write-host $global:branch
 
             $url = git-pr-parse "https://github.com/myuser/myrepo.git" $branch $base
             $url | should -be "https://github.com/myuser/myrepo/compare/$base...$branch"
